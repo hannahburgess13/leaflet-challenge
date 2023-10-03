9643800523d93b29b90ec2ccf761fed1db658797
hannahburgess13 <130720584+hannahburgess13@users.noreply.github.com>
2023-10-02 20:33:23 -0400

    Update logic.js

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -50,7 +50,7 @@ function createFeatures(earthquakemarker) {
 
 };
 
-// layera
+// layer
 let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
